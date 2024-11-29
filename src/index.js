44bb69c0dacf2094954d87efc40460ad49e02bbf
desk-059 <hacchobori-05@desk-059.local>
2024-11-29 18:00:41 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// function App() {
+//   return React.createElement("div", null, [
+//     React.createElement(
+//       "h1",
+//       { style: { color: "red" } },
+//       "This is a new world"
+//     ),
+//     React.createElement("h2", null, "everyone is nice person"),
+//     React.createElement(
+//       "p",
+//       null,
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos aliquam iure doloribus deserunt natus sint dicta ad dolor aperiam aspernatur."
+//     ),
+//   ]);
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <div>
+      <h1>My Profile</h1>
+      <p>My name is Alvin Liu</p>
+      <button>Check my profile</button>
+    </div>
+  );
+}
+
+ReactDOM.render(App(), document.querySelector("#root"));
